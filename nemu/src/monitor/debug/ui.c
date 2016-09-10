8ce454f518409d3a5b6de57db65f3c67f3454196
tracer-ics2016 <tracer@njuics.org>
2016-09-10 11:48:09 +0800

    >  compile NEMU 151242062 rainorangelemon Linux 1097b3356057 4.4.15-moby #1 SMP Thu Jul 28 22:03:07 UTC 2016 x86_64 GNU/Linux  11:48:09 up 14:34,  4 users,  load average: 0.04, 0.01, 0.00 ac8da9d288f413cc559a21907021dc739f587aeb

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -44,7 +44,9 @@ static int cmd_si(char *args){
 	}
 	sscanf(args,"%u",&si_number);
         if((sscanf(args,"%u",&si_number))>0)
-	    cpu_exec(si_number);
+ 	    { cpu_exec(si_number);
+	      printf("Running..\n");
+	    }
 	else
 	    printf("wrong number!\n");
 	return 0;	
