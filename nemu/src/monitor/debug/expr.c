a9985450bd722e5c98dde22679e130fc5d3e857c
tracer-ics2016 <tracer@njuics.org>
2016-09-15 22:32:40 +0800

    >  compile NEMU 151242062 rainorangelemon Linux 1097b3356057 4.4.15-moby #1 SMP Thu Jul 28 22:03:07 UTC 2016 x86_64 GNU/Linux  22:32:40 up 10:06,  6 users,  load average: 0.00, 0.00, 0.00 2245801fcd2f5f47bd94962ab1208a26a4669dbb

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -122,6 +122,7 @@ static bool make_token(char *e) {
 						strncpy(tokens[nr_token].str,substr_start+1,substr_len-1);
 						tokens[nr_token].str[substr_len-1]='\0';
 						nr_token++;
+						break;
 					default:
 						tokens[nr_token].type=rules[i].token_type;
 						tokens[nr_token].str[0]='\0';
