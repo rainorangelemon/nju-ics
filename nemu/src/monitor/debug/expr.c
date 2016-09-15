02c0c8e68af7acd01cb9cc4211b0eb295efa435f
tracer-ics2016 <tracer@njuics.org>
2016-09-15 14:44:19 +0800

    >  compile NEMU 151242062 rainorangelemon Linux 1097b3356057 4.4.15-moby #1 SMP Thu Jul 28 22:03:07 UTC 2016 x86_64 GNU/Linux  14:44:19 up  2:18,  4 users,  load average: 0.00, 0.00, 0.00 1f91f16bd00194a6e8d3271ee8fb2f43953bef0e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -22,7 +22,7 @@ static struct rule {
 	 * Pay attention to the precedence level of different rules.
 	 */
 
-	{"\\d+",Number},
+	{"\\d+",Number},			//number
        	{" +", NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
 	{"==", EQ}						// equal
