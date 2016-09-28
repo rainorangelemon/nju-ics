b378190390c1a6c71d999499a84da48d15b68c7d
tracer-ics2016 <tracer@njuics.org>
2016-09-28 15:49:31 +0000

    >  run 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  15:49:31 up 18:58,  2 users,  load average: 0.00, 0.00, 0.00 6d3af146d029f7f8626f10787320650dd4b411c4

--- nemu/src/cpu/exec/ctrl-trans/call-template.h
+++ nemu/src/cpu/exec/ctrl-trans/call-template.h
@@ -5,7 +5,7 @@
 static void do_execute(){
 	reg_l(R_ESP)=reg_l(R_ESP)-2;
 	MEM_W(reg_l(R_ESP),cpu.eip+DATA_BYTE+1);
-	cpu.eip=cup.eip+op_src->imm;
+	enp=eip+op_src->imm;
 	print_asm_template1();
 }
 
