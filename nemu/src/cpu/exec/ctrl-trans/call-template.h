c3fca15754c731479c750e5635b242403f6adc25
tracer-ics2016 <tracer@njuics.org>
2016-10-02 02:50:49 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  02:50:49 up 14:41,  2 users,  load average: 0.00, 0.00, 0.00 f4827ef54337a08945a025e39002709f3462ae3a

--- nemu/src/cpu/exec/ctrl-trans/call-template.h
+++ nemu/src/cpu/exec/ctrl-trans/call-template.h
@@ -3,9 +3,16 @@
 #define instr call
 
 static void do_execute(){
-	reg_l(R_ESP)=reg_l(R_ESP)-2;
-	MEM_W(reg_l(R_ESP),cpu.eip+DATA_BYTE+1);
-	cpu.eip=cpu.eip+op_src->imm;
+	if(ops_decoded.is_operand_size_16==true){
+		reg_l(R_ESP)=reg_l(R_ESP)-2;
+		uint16_t ip=(cpu.eip+DATA_BYTE+1)&(0x0000ffff);
+		MEM_W(reg_l(R_ESP),ip);
+		cpu.eip=((cpu.eip+op_src->val+DATA_BYTE+1)&(0x0000ffff))-DATA_BYTE-1;		
+	}else{
+		reg_l(R_ESP)=reg_l(R_ESP)-4;
+		MEM_W(reg_l(R_ESP),cpu.eip+DATA_BYTE+1);
+		cpu.eip=cpu.eip+op_src->val;
+	}
 	print_asm_template1();
 }
 
