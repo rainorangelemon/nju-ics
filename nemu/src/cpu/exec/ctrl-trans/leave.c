6d42dde730b8a3d40e49d057f45c0f23d8d916b5
tracer-ics2016 <tracer@njuics.org>
2016-10-07 11:42:45 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  11:42:45 up  2:56,  3 users,  load average: 0.00, 0.00, 0.00 c7782c931cce2166e996d32358a901dddde0616

---
+++ nemu/src/cpu/exec/ctrl-trans/leave.c
@@ -0,0 +1,14 @@
+#include "cpu/exec/helper.h"
+
+make_helper(leave){
+	if(ops_decoded.is_operand_size_16==true){
+		uint32_t val =swaddr_read(reg_l(R_ESP),2);
+		reg_l(R_ESP)=reg_l(R_ESP)+2;
+		reg_w(R_EBP)=val;	
+	}else{
+		int val = swaddr_read(reg_l(R_ESP),4);
+		reg_l(R_ESP)=reg_l(R_ESP)+4;
+		reg_l(R_EBP)=val;
+	}	
+	return 1;
+}
