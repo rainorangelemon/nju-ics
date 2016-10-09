1f672ffaf5e600a5158b752916b4072b9d76cabe
tracer-ics2016 <tracer@njuics.org>
2016-10-09 13:35:01 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  13:35:01 up 14:04,  3 users,  load average: 0.08, 0.02, 0.01 58cf33f2afea61ad92b627f76cb79f95181e621

---
+++ nemu/src/cpu/exec/ctrl-trans/jcc-template.h
@@ -0,0 +1,49 @@
+#include "cpu/exec/template-start.h"
+
+#define jcc_helper(name,condition) \
+make_helper(concat4(j,name,_,SUFFIX)) { \
+	int len=concat(decode_si_,SUFFIX) (eip+1); \
+	if(condition){ \
+		cpu.eip=cpu.eip+op_src->val; \
+	} \
+	if(ops_decoded.is_operand_size_16==true) \
+		cpu.eip=((cpu.eip+len+1)&(0xffff))-len-1; \
+	return len+1; \
+}
+
+jcc_helper(a,(cpu.flags.bits.cf==0)&&(cpu.flags.bits.zf==0))
+jcc_helper(ae,cpu.flags.bits.cf==0)
+jcc_helper(b,cpu.flags.bits.cf==1)
+jcc_helper(be,(cpu.flags.bits.cf==1)||(cpu.flags.bits.zf==1))
+jcc_helper(c,cpu.flags.bits.cf==1)
+jcc_helper(cxz,reg_w(R_ECX)==0)
+jcc_helper(ecxz,reg_l(R_ECX)==0)
+jcc_helper(e,cpu.flags.bits.zf)
+jcc_helper(g,(cpu.flags.bits.zf==0)&&(cpu.flags.bits.sf==cpu.flags.bits.of))
+jcc_helper(ge,cpu.flags.bits.sf==cpu.flags.bits.of)
+jcc_helper(l,cpu.flags.bits.sf!=cpu.flags.bits.of)
+jcc_helper(le,(cpu.flags.bits.zf==1)&&(cpu.flags.bits.sf!=cpu.flags.bits.of))
+jcc_helper(na,(cpu.flags.bits.cf==1)||(cpu.flags.bits.zf==1))
+jcc_helper(nae,(cpu.flags.bits.cf==1))
+jcc_helper(nb,(cpu.flags.bits.cf==0))
+jcc_helper(nbe,(cpu.flags.bits.cf==0)&&(cpu.flags.bits.zf==0))
+jcc_helper(nc,cpu.flags.bits.cf==0)
+jcc_helper(ne,cpu.flags.bits.zf==0)
+jcc_helper(ng,(cpu.flags.bits.zf==1)||(cpu.flags.bits.sf!=cpu.flags.bits.of))
+jcc_helper(nge,cpu.flags.bits.sf!=cpu.flags.bits.of)
+jcc_helper(nl,cpu.flags.bits.sf==cpu.flags.bits.of)
+jcc_helper(nle,(cpu.flags.bits.zf==0)&&(cpu.flags.bits.sf==cpu.flags.bits.of))
+jcc_helper(no,cpu.flags.bits.of==0)
+jcc_helper(np,cpu.flags.bits.pf==0)
+jcc_helper(ns,cpu.flags.bits.sf==0)
+jcc_helper(nz,cpu.flags.bits.zf==0)
+jcc_helper(o,cpu.flags.bits.of==1)
+jcc_helper(p,cpu.flags.bits.pf==1)
+jcc_helper(s,cpu.flags.bits.sf==1)
+jcc_helper(z,cpu.flags.bits.zf==1)
+jcc_helper(pe,cpu.flags.bits.pf==1)	
+jcc_helper(po,cpu.flags.bits.pf==0)
+
+#undef jcc_helper
+
+#include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/ctrl-trans/jcc.c
+++ nemu/src/cpu/exec/ctrl-trans/jcc.c
@@ -1,16 +1,46 @@
 #include "cpu/exec/helper.h"
 
-#define jcc_helper(name,length,condition) \
-make_helper(concat4(j,name,_,length)) { \
-	int len=concat(decode_i_,length) (eip+1); \
-	if(condition==1){ \
-		cpu.eip=cpu.eip+op_src->val; \
-	} \
-	return len+1; \
-}
+#define DATA_BYTE 1
+#include "jcc-template.h"
+#undef DATA_BYTE
 
-jcc_helper(e,b,cpu.flags.bits.zf)
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
 
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
 
+make_helper_v(ja)
+make_helper_v(jae)
+make_helper_v(jb)
+make_helper_v(jbe)
+make_helper_v(jc)
+make_helper_v(jcxz)
+make_helper_v(jecxz)
+make_helper_v(je)
+make_helper_v(jg)
+make_helper_v(jge)
+make_helper_v(jl)
+make_helper_v(jle)
+make_helper_v(jna)
+make_helper_v(jnae)
+make_helper_v(jnb)
+make_helper_v(jnbe)
+make_helper_v(jnc)
+make_helper_v(jne)
+make_helper_v(jng)
+make_helper_v(jnge)
+make_helper_v(jnl)
+make_helper_v(jnle)
+make_helper_v(jnp)
+make_helper_v(jns)
+make_helper_v(jnz)
+make_helper_v(jo)
+make_helper_v(jp)
+make_helper_v(jpe)
+make_helper_v(jpo)
+make_helper_v(js)
+make_helper_v(jz)
 
-#undef jcc_helper
