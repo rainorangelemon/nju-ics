f0823b7dea047ce5c2678a512be28d8a98327d25
tracer-ics2016 <tracer@njuics.org>
2016-10-09 11:20:02 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  11:20:02 up 12:07,  4 users,  load average: 0.09, 0.05, 0.01 cdc2a3657883abb7ff0db63cb06915de2fe6bf03

--- nemu/src/cpu/exec/ctrl-trans/jmp-template.h
+++ nemu/src/cpu/exec/ctrl-trans/jmp-template.h
@@ -11,6 +11,7 @@ make_helper(concat(jmp_rel_,SUFFIX)){
 	return len+1;
 }
 
+#if ((DATA_BYTE==2) || (DATA_BYTE==4))
 make_helper(concat(jmp_rm_,SUFFIX)){
 	concat(decode_rm_,SUFFIX)(eip+1);
 	if(ops_decoded.is_operand_size_16==true)
@@ -19,6 +20,6 @@ make_helper(concat(jmp_rm_,SUFFIX)){
 		cpu.eip=op_src->val;
 	return 0;
 }
-
+#endif
 
 #include "cpu/exec/template-end.h"
