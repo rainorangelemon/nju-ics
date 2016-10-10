d8c8851afa5e8606ff64b1bd166e7e06cd2b1632
tracer-ics2016 <tracer@njuics.org>
2016-10-10 01:43:28 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  01:43:28 up 16:40,  2 users,  load average: 0.24, 0.05, 0.02 c605840f1bcc02ab427fef78fcef685290cc75a

---
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -0,0 +1,21 @@
+#include "cpu/exec/template-start.h"
+
+#define instr stos
+
+static void do_execute(){
+	DATA_TYPE src_index=concat(reg_,SUFFIX)(R_EAX);
+	uint32_t dest_index=reg_l(R_EDI);
+	MEM_W(dest_index,src_index);
+	if(cpu.flags.bits.df==0){
+		reg_l(R_EDI)=reg_l(R_EDI)+DATA_BYTE;
+	}else{
+		reg_l(R_EDI)=reg_l(R_EDI)-DATA_BYTE;	
+	}
+}
+
+make_helper(concat3(instr,_,SUFFIX)){
+	do_execute();
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
