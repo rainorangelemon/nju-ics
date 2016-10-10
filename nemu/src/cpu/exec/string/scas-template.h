1d48bfa651f08b032c9bc93012055a770f3e0e1a
tracer-ics2016 <tracer@njuics.org>
2016-10-10 02:12:35 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  02:12:35 up 17:10,  2 users,  load average: 0.00, 0.00, 0.00 eb9e7917d1edcaf6b0bd1e0a4e6932af2a15b79

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -5,7 +5,15 @@
 static void do_execute(){
 	int IncDec=0;
 	uint32_t src_index=reg_l(R_EDI);
-	MEM_W(REG(R_EAX),MEM_R(src_index));
+	DATA_TYPE result=MEM_R(src_index);
+	result=~result;
+	DATA_TYPE sum=result+1+REG(R_EAX);
+	set_of(REG(R_EAX),result,DATA_BYTE,1);
+	set_sf(sum,DATA_BYTE);
+	set_zf(sum);
+	set_pf(sum);
+	set_cf(REG(R_EAX),result,1,DATA_BYTE);
+	set_af(REG(R_EAX),result,1);
 	if(cpu.flags.bits.df==0){
 		IncDec=DATA_BYTE;
 	}else{
