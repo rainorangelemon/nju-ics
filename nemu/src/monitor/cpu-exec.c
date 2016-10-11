38d4a7f4fef5ac4e2b6776f10a40d40ae176a713
tracer-ics2016 <tracer@njuics.org>
2016-10-11 08:25:32 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 8a9fad57e5c0 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  08:25:32 up 10:43,  4 users,  load average: 0.01, 0.02, 0.00 5730441b671a19355ba7489ca4abfcd49d5cf61

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -9,7 +9,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 128
+#define MAX_INSTR_TO_PRINT 270
 
 int nemu_state = STOP;
 
