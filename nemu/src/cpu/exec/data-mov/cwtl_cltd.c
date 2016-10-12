182bc8146e3f573c3808290fb00c9ab15b3cddf0
tracer-ics2016 <tracer@njuics.org>
2016-10-12 01:18:10 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 1bf164d27dcd 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  01:18:10 up  1:23,  4 users,  load average: 0.00, 0.00, 0.00 a075bc84102215a746d10d1c9a95f09daaeb29

--- nemu/src/cpu/exec/data-mov/cwtl_cltd.c
+++ nemu/src/cpu/exec/data-mov/cwtl_cltd.c
@@ -2,13 +2,13 @@
 
 make_helper(cwtl_cltd){
 	if(ops_decoded.is_operand_size_16){
-		if(reg_w(R_EAX)<0){
+		if((int16_t)reg_w(R_EAX)<0){
 			reg_w(R_EDX)=0xffff;
 		}else{
 			reg_w(R_EDX)=0;
 		}
 	}else{
-		if(reg_l(R_EAX)<0){
+		if((int32_t)reg_l(R_EAX)<0){
 			reg_l(R_EDX)=0xffffffff;
 		}else{
 			reg_l(R_EDX)=0;
