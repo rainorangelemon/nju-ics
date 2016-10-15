891b21cf2bbd8ec2dc24b01220e63f779ae2004c
tracer-ics2016 <tracer@njuics.org>
2016-10-15 02:48:55 +0000

    >  compile NEMU 151242062 rainorangelemon Linux 1bf164d27dcd 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  02:48:55 up 1 day, 13:35,  4 users,  load average: 0.08, 0.02, 0.01 23211ef5f2ef7cb8c4e92bc52edfa62b169f4fdf

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -5,20 +5,14 @@
 
 int main() {
 	FLOAT a = f2F(0.5);
-	nemu_assert(a==0x8000);
 	FLOAT b = f2F(0.5);
 	FLOAT c = f2F(-0.5);
-	nemu_assert(c==0xffff8000);
 
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
-	nemu_assert(dt==0x14000);
 	FLOAT sqrt_dt = sqrt(dt);
-	nemu_assert(sqrt_dt==0x11e37);
 
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
-	nemu_assert(x1==0x9e37);
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
-	nemu_assert(x2==0xfffe61c9);
 
 	FLOAT x1_ans = f2F(0.618);
 	FLOAT x2_ans = f2F(-1.618);
