85292d4e861d3c066f234c1bfc7b76da2ee2476e
tracer-ics2016 <tracer@njuics.org>
2016-11-16 13:21:08 +0000

    >  compile NEMU 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  13:21:08 up 1 day, 14:32,  3 users,  load average: 0.00, 0.00, 0.00 97603308b42218d265d7a896882fb5d3c8773615

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,16 +1,156 @@
 #include "common.h"
+#include <stdlib.h>
+#include "burst.h"
+#include "cpu/reg.h"
+#define data_size 64
+#define L1_size 64*1024
+#define L2_size 4*1024*1024
+#define L1_way 8
+#define L2_way 16
+#define L1_group_number L1_size/(L1_way*data_size)
+#define L2_group_number L2_size/(L2_way*data_size)
+void ddr3_read(hwaddr_t, void*);
+void ddr3_write(hwaddr_t, void*,uint8_t*);
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
+
+
+
+struct Cache_L1{
+	bool v;
+	uint32_t tag;
+	uint8_t data[data_size];
+}cache1[L1_size/data_size];
+
+struct Cache_L2{
+	bool d;
+	bool v;
+	uint32_t tag;
+	uint8_t data[data_size];
+}cache2[L2_size/data_size];
+
+uint32_t L2_read(uint32_t addr){
+	uint32_t group_number = (addr >> 6)&(L2_group_number-1); /*if data_size is not 64, here needs to be modified.*/
+	int i=group_number*(L2_size/(data_size*L2_group_number));
+	int empty_index=-1;
+	int j;
+	for(j=0;j<L2_way;j++){
+		if(((cache2[i+j].tag)==(addr>>18))&&(cache2[i+j].v=1)){  /*if data_size is not 64b and L2_size changed, here needs to be modified.*/
+			return i+j;
+		}
+		if(cache2[i+j].v==0){
+			empty_index=i+j;
+		}
+	}
+	if(empty_index==-1){
+		j = (rand() % L2_way); 
+	}
+	if(cache2[i+j].d==1){
+		int k;
+		int mask_size=BURST_LEN+1;
+		uint8_t mask[mask_size];
+		memset(mask,1,mask_size+1);
+		for(k=0;k<(data_size/BURST_LEN);k++)
+			ddr3_write(((addr>>6)<<6)+k*BURST_LEN, cache2[i+j].data+k*BURST_LEN,mask);
+	}
+	cache2[i+j].d=false;
+	cache2[i+j].v=true;
+	cache2[i+j].tag=addr>>18;
+	int k;
+	for(k=0;k<data_size/BURST_LEN;k++){
+		ddr3_read(((addr>>6)<<6)+k*BURST_LEN, cache2[i+j].data+k*BURST_LEN);
+	}
+	return i+j;
+}
+
+
+uint32_t L1_read(uint32_t addr){
+	uint32_t group_number = (addr >> 6)&(L1_group_number-1); /*if data_size is not 64, here needs to be modified.*/
+	int i=group_number*(L1_size/(data_size*L1_group_number));
+	int empty_index=-1;
+	int j;
+	for(j=0;j<L1_way;j++){
+		if(((cache1[i+j].tag)==(addr>>13))&&(cache1[i+j].v=1)){  /*if data_size is not 64b and L1_size changed, here needs to be modified.*/
+			return i+j;
+		}
+		if(cache1[i+j].v==0){
+			empty_index=i+j;
+		}
+	}
+	int L2_index=L2_read(addr);
+	if(empty_index==-1){
+		j = (rand() % L1_way); 
+	}
+	cache1[i+j].v=true;
+	memcpy(cache1[i+j].data,cache2[L2_index].data,data_size);
+	cache1[i+j].tag=addr>>13;
+	return i+j;
+}
+
+void L2_write(uint32_t addr, uint32_t len, uint32_t data){
+	uint32_t group_number = (addr >> 6)&(L2_group_number-1); /*if data_size is not 64, here needs to be modified.*/
+        int i=group_number*(L2_size/(data_size*L2_group_number));
+	int index = addr & (data_size-1);
+	int j;
+	for(j=0;j<L2_way;j++){
+		if(((cache2[i+j].tag)==(addr>>18))&&(cache2[i+j].v=1)){  /*if data_size is not 64b and L2_size changed, here needs to be modified.*/
+			memcpy(cache2[i+j].data+index,&data,len);
+			cache2[i+j].d=true;
+			return ;
+		}
+	}
+	int L2_index=L2_read(addr);
+	memcpy(cache2[L2_index].data+index,&data,len);
+	cache2[L2_index].d=false;
+	int k;
+	int mask_size=BURST_LEN+1;
+	uint8_t mask[mask_size];
+	memset(mask,1,mask_size+1);
+	for(k=0;k<len;k++)
+		ddr3_write(((addr>>6)<<6)+k*BURST_LEN+index, cache2[L2_index].data+index+k*BURST_LEN,mask);
+	return ;
+}
+
+void L1_write(uint32_t addr, uint32_t len, uint32_t data){
+        uint32_t group_number = (addr >> 6)&(L1_group_number-1); /*if data_size is not 64, here needs to be modified.*/
+        int i=group_number*(L1_size/(data_size*L1_group_number));
+	int index = addr & (data_size-1);
+	int j;
+	for(j=0;j<L1_way;j++){
+		if(((cache1[i+j].tag)==(addr>>13))&&(cache1[i+j].v=1)){  /*if data_size is not 64b and L1_size changed, here needs to be modified.*/
+			memcpy(cache1[i+j].data+index,&data,len);
+			break;
+		}
+	}
+	L2_write(addr,len,data);
+}
+
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	int index = addr & (data_size-1);
+	uint8_t piece[4];
+	memset(piece,0,sizeof(piece));
+	int L1_index = L1_read(addr);
+	if(data_size<=index+len){
+		memcpy(piece,cache1[L1_index].data+index,data_size-index);
+		int L1_index_ = L1_read(addr+len);
+		memcpy(piece+(data_size-index),cache1[L1_index_].data,len-(data_size-index));
+	}else{
+		memcpy(piece,cache1[L1_index].data+index,len);
+	}
+	/*
+	uint32_t result=(unalign_rw(piece,4)) & (~0u >> ((4 - len) << 3));
+	return result;*/
+	int zero=0;
+	uint32_t tmp = unalign_rw(piece + zero, 4) & (~0u >> ((4 - len) << 3)); 
+	return tmp;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	dram_write(addr, len, data);
+	L1_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
