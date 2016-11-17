f630a798be6b7db428622cf761894c6efce6f251
tracer-ics2016 <tracer@njuics.org>
2016-11-17 12:48:38 +0000

    >  compile NEMU 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  12:48:38 up  3:00,  3 users,  load average: 0.00, 0.00, 0.00 c6e24eab4de9c3fbcb55a3f2f34ac12bffae458

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -110,6 +110,7 @@ void cmd_x(unsigned int area_size,unsigned int result){
 	        unsigned int memory_number;
 	        unsigned int i;
 		for(i=0; i<area_size; i++){
+			printf("shit");
 			memory_number = swaddr_read(result,4);          		 	    
 			printf("0x%x:  0x",result);
 			printf("%08x",memory_number);
