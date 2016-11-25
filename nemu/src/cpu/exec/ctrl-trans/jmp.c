8cdf20be8e88a75604145b1a5997689c24c7a5d0
tracer-ics2016 <tracer@njuics.org>
2016-11-25 15:41:44 +0000

    >  compile NEMU 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  15:41:44 up 10:33,  3 users,  load average: 0.00, 0.00, 0.00 406fec75f28c9832399dd3aaa1cd7bc5efd40cfd

--- nemu/src/cpu/exec/ctrl-trans/jmp.c
+++ nemu/src/cpu/exec/ctrl-trans/jmp.c
@@ -14,3 +14,15 @@
 
 make_helper_v(jmp_rel)
 make_helper_v(jmp_rm)
+
+void load_seg(uint8_t sreg);
+
+make_helper(ljmp){
+	unsigned short dest=instr_fetch(cpu.eip+5,2);
+	swaddr_t jump=instr_fetch(cpu.eip+1,4);
+	seg(R_CS).selector.val=dest;
+	seg(R_CS).dirty=false;
+	load_seg(R_CS);
+	cpu.eip=jump-7;
+	return 7;
+}
