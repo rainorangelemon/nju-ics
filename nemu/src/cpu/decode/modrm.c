c1e0a7e1199b76846aa376bfc6370c5b27019c34
tracer-ics2016 <tracer@njuics.org>
2016-12-04 04:17:24 +0000

    >  compile NEMU 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  04:17:24 up 12:57,  4 users,  load average: 0.00, 0.00, 0.00 c7b7517ad2ea4ad2b8a6079c9785ac79dd963f0

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -114,6 +114,7 @@ int read_ModR_M(swaddr_t eip, Operand *rm, Operand *reg) {
 	}
 	else {
 		int instr_len = load_addr(eip, &m, rm);
+		printf("%d\n",rm->sreg);
 		rm->val = swaddr_read(rm->addr, rm->size, rm->sreg);
 		return instr_len;
 	}
