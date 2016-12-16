c8819088dbfaabfdfea02eb2827473984cb8eea1
tracer-ics2016 <tracer@njuics.org>
2016-12-16 16:00:37 +0000

    >  compile NEMU 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  16:00:37 up 17:21,  3 users,  load average: 0.02, 0.01, 0.00 4d9c277551fe5aab411254750306716a9e105e8

--- nemu/src/cpu/exec/io/in-template.h
+++ nemu/src/cpu/exec/io/in-template.h
@@ -4,7 +4,7 @@ uint32_t pio_read(ioaddr_t addr,size_t len);
 make_helper(concat(in_d_,SUFFIX)){
 	uint16_t index=reg_w(R_DX);
 	REG(R_EAX)=(DATA_TYPE)pio_read(index,DATA_BYTE);
-	print_asm("in_d");
+	printf("in:  index:0x%x eax:0x%x\n",index,(DATA_TYPE)REG(R_EAX));
 	return 1;
 }
 
@@ -12,7 +12,7 @@ make_helper(concat(in_d_,SUFFIX)){
 make_helper(concat(in_i_,SUFFIX)){
 	uint16_t index=instr_fetch(eip+1,1);
 	REG(R_EAX)=(DATA_TYPE)pio_read(index,DATA_BYTE);
-	print_asm("in_i");
+	printf("in:  index:0x%x eax:0x%x\n",index,(DATA_TYPE)REG(R_EAX));
 	return 2;
 }
 
--- nemu/src/cpu/exec/io/out-template.h
+++ nemu/src/cpu/exec/io/out-template.h
@@ -5,7 +5,7 @@ void pio_write(ioaddr_t addr,size_t len, uint32_t data);
 make_helper(concat(out_d_,SUFFIX)){
 	uint16_t index=reg_w(R_DX);
 	pio_write(index,DATA_BYTE,(DATA_TYPE)REG(R_EAX));
-	print_asm("out_d");
+	printf("out:  index:0x%x eax:0x%x\n",index,(DATA_TYPE)REG(R_EAX));
 	return 1;
 }
 
@@ -13,7 +13,7 @@ make_helper(concat(out_d_,SUFFIX)){
 make_helper(concat(out_i_,SUFFIX)){
 	uint16_t index=instr_fetch(eip+1,1);
 	pio_write(index,DATA_BYTE,(DATA_TYPE)REG(R_EAX));
-	print_asm("out_i");
+	printf("out:  index:0x%x eax:0x%x\n",index,(DATA_TYPE)REG(R_EAX));
 	return 2;
 }
 
