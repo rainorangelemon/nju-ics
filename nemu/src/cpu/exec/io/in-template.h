88df18beb2147797281341bdec0c1fa4430131c4
tracer-ics2016 <tracer@njuics.org>
2016-12-16 15:02:58 +0000

    >  compile NEMU 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  15:02:58 up 16:24,  3 users,  load average: 0.08, 0.04, 0.05 3337fd1f8235cc43b81973c11897a08f70807d8

--- nemu/src/cpu/exec/io/in-template.h
+++ nemu/src/cpu/exec/io/in-template.h
@@ -3,7 +3,7 @@
 uint32_t pio_read(ioaddr_t addr,size_t len);
 make_helper(concat(in_d_,SUFFIX)){
 	uint16_t index=reg_w(R_DX);
-	REG(R_EAX)=(uint32_t)(DATA_TYPE)pio_read(index,DATA_BYTE);
+	REG(R_EAX)=(DATA_TYPE)pio_read(index,DATA_BYTE);
 	print_asm("in_d");
 	return 1;
 }
@@ -11,7 +11,7 @@ make_helper(concat(in_d_,SUFFIX)){
 
 make_helper(concat(in_i_,SUFFIX)){
 	uint16_t index=instr_fetch(eip+1,1);
-	REG(R_EAX)=(uint32_t)(DATA_TYPE)pio_read(index,DATA_BYTE);
+	REG(R_EAX)=(DATA_TYPE)pio_read(index,DATA_BYTE);
 	print_asm("in_i");
 	return 2;
 }
