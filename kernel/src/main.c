0fe4aa2ac657228485f87b61d01aeb6474c37f7d
tracer-ics2016 <tracer@njuics.org>
2016-12-17 11:36:05 +0000

    >  compile kernel 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  11:36:05 up 1 day,  1:36,  2 users,  load average: 0.10, 0.08, 0.02 5743a64d77bd4f37e20436769112c2c0e352b107

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -71,8 +71,7 @@ void init_cond() {
 	 * Note that the output is actually performed only when
 	 * the serial port is available in NEMU.
 	 */
-	Log("Hello, NEMU world! I love this world and these violent delights");
-	Log("Smartgirlsarejustmytype!!!");
+	Log("Hello, NEMU world!"); 
 
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
