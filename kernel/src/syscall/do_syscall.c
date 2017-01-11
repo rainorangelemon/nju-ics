fa49a63d9b584db85fdc73ea3321a4dc7e3c5ee4
tracer-ics2016 <tracer@njuics.org>
2017-01-11 11:41:43 +0000

    >  compile kernel 151242062 rainorangelemon Linux d02971b42c49 4.4.20-moby #1 SMP Thu Sep 15 12:10:20 UTC 2016 x86_64 GNU/Linux  11:41:43 up 1 day,  9:33,  2 users,  load average: 0.16, 0.03, 0.02 765f43b139b55246df5e7857da54ac5b34d8632a

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -4,6 +4,7 @@
 void add_irq_handle(int, void (*)(void));
 uint32_t mm_brk(uint32_t);
 int fs_ioctl(int, uint32_t, void *);
+void serial_printc(char);
 
 static void sys_brk(TrapFrame *tf) {
 #ifdef IA32_PAGE
@@ -16,21 +17,22 @@ static void sys_ioctl(TrapFrame *tf) {
 	tf->eax = fs_ioctl(tf->ebx, tf->ecx, (void *)tf->edx);
 }
 
-/*static void sys_write(TrapFrame *tf){
-	if(tf->ebx==1||tf->ebx==2){
-		int count;
-		for(count=0;count<tf->edx;count++)
-			serial_printc(((char*)tf->ecx)[count]);
-		tf->eax=tf->edx;
-	}
-}*/
-
 int fs_write(int fs,void *buf,int len);
 int fs_open(const char *pathname,int flags);
 int fs_read(int fd,void *buf,int len);
 int fs_close(int fd);
 int fs_lseek(int fd,int offset,int whence);
 
+static inline void sys_write(TrapFrame *tf){
+	if(tf->ebx==1||tf->ebx==2){
+		int count;
+		for(count=0;count<tf->edx;count++)
+			serial_printc(((char*)tf->ecx)[count]);
+		tf->eax=tf->edx;
+	}else{
+		tf->eax=fs_write(tf->ebx,(void*)tf->ecx,tf->edx);
+	}
+}
 void do_syscall(TrapFrame *tf) {
 	switch(tf->eax) {
 		/* The `add_irq_handle' system call is artificial. We use it to
@@ -46,7 +48,7 @@ void do_syscall(TrapFrame *tf) {
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_ioctl: sys_ioctl(tf); break;
 		case SYS_write: 
-			tf->eax=fs_write(tf->ebx,(void *)tf->ecx,tf->edx);break;
+			sys_write(tf);break;
 		case SYS_open: 
 			tf->eax=fs_open((const char *)tf->ebx,tf->ecx);break;
 		case SYS_read: 
